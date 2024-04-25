7ddd1fdd5db70fbcc5b54a162a24b356eec6cb6b
carris kevin <oceane.edde@gmx.fr>
2024-04-25 23:45:43 +0200

    initialisation

--- php/inscription.php
+++ php/inscription.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 } else {
     
-    header("Location: index.php");
+    header("Location: connexion.php");
     exit();
 }
 ?>
--- sql/connexion.sql
+++ sql/connexion.sql
@@ -1,1 +1,9 @@
-INSERT INTO administrateurs (username, email, password) VALUES ('admin', 'admin@example.com', 'motdepasse');
+CREATE TABLE administrateurs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO administrateurs (username, email, password) 
+VALUES ('administrateur', 'admin@exemple.com', 'administrateur');
